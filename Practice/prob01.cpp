2e8b651e1e1b3c5d5ba890387d8c5bca1cbe38e2
gittykrish <kantkrishna663@gmail.com>
2021-01-10 19:53:55 +0530

    every element appears three times except one ,which appears exactly one . Find that single one

--- Practice/prob01.cpp
+++ Practice/prob01.cpp
@@ -1,6 +1,5 @@
 /*
-    Given a non empty array of interger , every element appears three times except one ,
-    which appears exactly one . Find that single one.
+    Given a non empty array of interger , every element appears three times except one ,which appears exactly one . Find that single one.
     Input : [2,2,3,2]       output: 3
     Input : [0,1,0,1,0,99]  Output : 99
 */
@@ -8,7 +7,8 @@
 using namespace std;
 int main(){
     
-    int t; cin>>t;
+    int t; 
+    cin>>t;
     while(t--){
         int n; 
         cin>>n;
