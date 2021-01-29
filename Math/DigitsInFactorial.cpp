c68b7041e77a8b5478a153a2699818b088191147
gittykrish <kantkrishna663@gmail.com>
2021-01-29 23:57:01 +0530

    Digits In factorial

---
+++ Math/DigitsInFactorial.cpp
@@ -0,0 +1,27 @@
+/*
+Given an integer N. Find the number of digits that appear in its factorial. 
+Factorial is defined as, factorial(n) = 1*2*3*4……..*N and factorial(0) = 1.
+ 
+
+Example 1:
+
+Input: N = 5
+Output: 3
+Explanation: Factorial of 5 is 120.
+Number of digits in 120 is 3 (1, 2, and 0)
+ 
+
+
+*/
+
+
+int digitsInFactorial(int N)
+{
+    
+    double sum=0.0; 
+    for(int j=1;j<=N;j++)
+    {
+        sum+=log10(j);
+    }
+    return(1+floor(sum));
+}
